603501db12b39def1ba030ac4a09ba2c59291cae
Leandro Batista <leandrobouwierphp@gmail.com>
2025-07-15 16:42:15 -0300

    inicio do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-
+import { Button } from "./components/Button";
 export function App() {
   return (
-    <h1>Hellow World</h1>
-  )
+    <>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+    </>
+  );
 }
-
-
