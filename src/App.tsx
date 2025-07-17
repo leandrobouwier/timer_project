db19bd14d5dcbc1c983c103e9e774df4beab95d9
Leandro Batista <leandrobouwierphp@gmail.com>
2025-07-17 16:52:41 -0300

    react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { Button } from "./components/Button";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
 export function App() {
   return (
-    <>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      
+
+    </ThemeProvider>
   );
 }
